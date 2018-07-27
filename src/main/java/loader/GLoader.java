190f836f65aff39cd0198d2852e1dd06616e087b
Kest <k.sharipo@mail.ru>
2018-07-27 15:55:33 +0300

    add luminosity coordinates

--- src/main/java/loader/GLoader.java
+++ src/main/java/loader/GLoader.java
@@ -6,40 +6,23 @@ import java.io.*;
 import java.util.ArrayList;
 
 class GLoader2 {
-    public static void main(String[] args) {
-        String fileName = "test.json";
-        ArrayList<Radiant> radiants = GLoader.getRadiants(fileName);
-        //тест загрузки источников
-        System.out.println("Источники: " + radiants.size());
-        for (int i=0;i<radiants.size();i++) {
-            System.out.print(radiants.get(i).getName()+"\t");
-            System.out.println(radiants.get(i).getiMax());
-        }
-        //тест загрузки приемников
-        ArrayList<Receiver> receivers = GLoader.getReceivers(fileName);
-        System.out.println("Приемники: " + receivers.size());
-        for (int i=0;i<receivers.size();i++) {
-            System.out.print(receivers.get(i).getSensitivity()+"\t");
-            System.out.println(receivers.get(i).getResponseRate());
-        }
-    }
+
 }
 
 public class GLoader {
     //Класс для метода fromJson()
-    private class GLoaderInit {
-        ArrayList<Radiant> Radiants;
-        ArrayList<Receiver> Receivers;
-    }
+    private ArrayList<Radiant> Radiants;
+    private ArrayList<Receiver> Receivers;
+
     //Метод для чтения данных
-    private static GLoaderInit getAll(String fileName) {
+    private static GLoader getAll(String fileName) {
         //Считывание файла в строку
         try (FileReader jsonData = new FileReader(fileName))
         {
             //Инициализация Gson
             Gson gsonObject = new GsonBuilder().create();
             //Возврат всех значений
-            return gsonObject.fromJson(jsonData, GLoaderInit.class);
+            return gsonObject.fromJson(jsonData, GLoader.class);
 
         } catch(IOException ex) {
             System.out.println("Файл "+fileName+" не найден");
